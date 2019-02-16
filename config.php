bec1781278f3b419b8c0a594485a9dbb05876255
yusuffatal <31797247+yusuffatal@users.noreply.github.com>
2019-02-16 13:17:09 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 //android_id
-$anid="xxxxxxxxxxxxxxx";
+$anid="dc1dc16bf20c8f7e";
 //data sign
-$sign="xxxxxxxxxxxxxxx";
+$sign="2444d2b9298589fbe3336d10d47c0b177a3ebec9";
 //data aid jika tidak ada kosongkan
 $aid="xxxxxxxxxxxxxxx";
 //data uuid
-$uuid="xxxxxxxxxxxxxxx";
+$uuid="8978c64ab63b800890b4a50ecd966af7107af8eb";
 
 ?>
